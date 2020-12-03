002bee9e0c9272fefbcdf93afa384a963424076c
TPReal <tpreal@gmail.com>
2020-12-03 11:31:32 +0000

    2020/d01..d03

--- app.ts
+++ app.ts
@@ -1,24 +1,3 @@
-import * as y2019d02 from './src/2019/d02';
-import * as y2019d05 from './src/2019/d05';
-import * as y2019d06 from './src/2019/d06';
-import * as y2019d07 from './src/2019/d07';
-import * as y2019d08 from './src/2019/d08';
-import * as y2019d09 from './src/2019/d09';
-import * as y2019d10 from './src/2019/d10';
-import * as y2019d11 from './src/2019/d11';
-import * as y2019d12 from './src/2019/d12';
-import * as y2019d13 from './src/2019/d13';
-import * as y2019d14 from './src/2019/d14';
-import * as y2019d15 from './src/2019/d15';
-import * as y2019d16 from './src/2019/d16';
-import * as y2019d17 from './src/2019/d17';
-import * as y2019d18 from './src/2019/d18';
-import * as y2019d19 from './src/2019/d19';
-import * as y2019d20 from './src/2019/d20';
-import * as y2019d21 from './src/2019/d21';
-import * as y2019d22 from './src/2019/d22';
-import * as y2019d23 from './src/2019/d23';
-import * as y2019d24 from './src/2019/d24';
-import * as y2019d25 from './src/2019/d25';
+import * as y2020d03 from './src/2020/d03';
 
-y2019d25.run();
+y2020d03.run();
